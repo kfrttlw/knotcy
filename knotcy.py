b738325af9ffc5fd38d6a726e01d2a61af1784d9
kfrt <kfrtlwisfu@gmail.com>
2026-02-15 17:32:02 +0300

    Код адаптирован согласно стандартам Flake8.

--- knotcy.py
+++ knotcy.py
@@ -1,13 +1,13 @@
 import os
 import sqlite3
 import time
-import random
-import string
+# import random
+# import string
 from datetime import datetime
 
 
-#---------forcomfy------------------------------1
-rise = r""" ____  __.              __                
+# ---------forcomfy------------------------------1
+rise = r""" ____  __.              __
 |    |/ _| ____   _____/  |_  ____ ___.__.
 |      <  /    \ /  _ \   __\/ ___<   |  |
 |    |  \|   |  (  <_> )  | \  \___\___  |
@@ -15,8 +15,10 @@ rise = r""" ____  __.              __
         \/    \/                 \/\/"""
 slash = '=' * 20 + '<>' + '=' * 20
 
+
 def clear_all():
     os.system('cls' if os.name == 'nt' else 'clear')
+
 
 def start_bunner(info=""):
     clear_all()
@@ -25,13 +27,20 @@ def start_bunner(info=""):
         print(info)
 
 
-def proverka_un_name(tab_nam): 
-    cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name=?", (tab_nam,))
+def proverka_un_name(tab_nam):
+    cur.execute("""
+                SELECT name
+                FROM sqlite_master
+                WHERE type='table' AND name=?""",
+                (tab_nam,))
     return cur.fetchone() is not None
-#---------forcomfy------------------------------2
-# ---------------------------------ХЗдесь все с sql 1 пункт------------------------------------1
+# ---------forcomfy------------------------------2
+# ---------------------------------ХЗдесь все с sql 1 пункт------------------1
+
+
 connnn = sqlite3.connect('knotcy_base.sqlite')
 cur = connnn.cursor()
+
 
 # для создания папки задач
 def add_new_table(name):
@@ -46,7 +55,6 @@ def add_new_table(name):
         print(f'Table, {clean_name} - already there.')
         # input('press any button for back menu...')
     else:
-        
         try:
             cur.execute(f'''
         CREATE TABLE IF NOT EXISTS "{clean_name}"(
@@ -62,38 +70,30 @@ def add_new_table(name):
             print(f"error create table - {clean_name}: {e}")
     input('press any button for back menu...')
 
-# ---------------------------------------------------------------------------------------------2 
+# ----------------------------------------------------------------------------2
+# ---------------------------------ХЗдесь все для sql 2 пункт---------------1
 
-# ---------------------------------ХЗдесь все для sql 2 пункт------------------------------------1
-def view_task_folder(): # показ всех таблиц 
-    
+
+def view_task_folder():  # показ всех таблиц
     try:
         cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
-
-
         all_tables = [tab[0] for tab in cur.fetchall()]
-
         if all_tables:
             print("all_tables:")
             for w, table_all_name in enumerate(all_tables, 1):
                 print(f'[{w}] -> {table_all_name}')
             return all_tables
-            
-            
-            # for tabl_all in all_tables:
-            #     print(f'[0] -> {tabl_all[0]}')
-            
         else:
             print('You dont have any folders with task.')
             return []
-            # input('press any button for back menu...') 
+            # input('press any button for back menu...')
     except sqlite3.Error as a:
         print(f'Error when viewing folders task: {a}')
-        return []    
-    # input('press any button for back menu...') 
+        return []
+# ---------------------------------------------------------------------2
+# ---------------------------------ХЗдесь все c sql 2.2 пункт---------1
 
-# ---------------------------------------------------------------------------------------------2
-# ---------------------------------ХЗдесь все c sql 2.2 пункт------------------------------------1
+
 def redact_view_folder():
     help_help = ""
     while True:
@@ -103,7 +103,10 @@ def redact_view_folder():
             print(help_help)
             help_help = ""
 
-        action_folder = input('Please select an number or name a folder, or (!quit):\n->') 
+        action_folder = input(
+            'Please select an number or name a folder,'
+            'or (!quit):\n->'
+        )
 
         if action_folder.strip() in ['!q', '!quit', '!ex', '!exit']:
             break
@@ -118,60 +121,69 @@ def redact_view_folder():
 
         if proverka_un_name(action_folder):
             start_bunner()
-            
-
-
-            new_name_redacted = input(f'Redacting <{action_folder}> enter a new name a table or (!q)\n->')
+            new_name_redacted = input(
+                f'Redacting <{action_folder}>'
+                f'enter a new name a table or (!q)\n->'
+            )
             if new_name_redacted in ['!q', '!quit', '!ex', '!exit']:
                 continue
-            
             else:
-                if new_name_redacted:                                
-                    clean_name = ''.join(e for e in new_name_redacted if e.isalnum() or e == '_')
-                    cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name=?", (clean_name,))
-                    if cur.fetchone():        
-                        input(f'A folder with that name - {clean_name}, already exists\nPress any button to back menu...')
-                    else:    
+                if new_name_redacted:
+                    clean_name = ''.join(e for e in new_name_redacted
+                                         if e.isalnum() or e == '_')
+                    cur.execute("""SELECT name
+                                FROM sqlite_master
+                                WHERE type='table' AND name=?""",
+                                (clean_name,))
+                    if cur.fetchone():
+                        input(
+                            f'Folder with name - {clean_name}, already exists'
+                            '\nPress any button to back menu...'
+                            )
+                    else:
                         if not clean_name:
-                            input('Error: name is empty.\nPress any button to back menu...')
-                            
-                        # крч здесь доделать логику имя принимает маску но если такое имя существует то маска остается навсеглдда 
+                            input(
+                                'Error: name is empty.'
+                                '\nPress any button to back menu...'
+                                )
+
                         else:
                             try:
-                            # one_name = redact_action.replace('"', '""')
-                            # two_name = clean_name.replace('"', '""')
-                                cur.execute(f'ALTER TABLE "{action_folder}" RENAME TO "{clean_name}"')
+                                #  one_name = redact_action.replace('"', '""')
+                                #  two_name = clean_name.replace('"', '""')
+                                cur.execute(f'''
+                                            ALTER TABLE "{action_folder}"
+                                            RENAME TO "{clean_name}"''')
                                 connnn.commit()
-                                input(f'Name changed successfully.\nNew name - <{clean_name}>\nPress any button to back...')
+                                input(
+                                    'Name changed successfully.'
+                                    f'\nNew name - <{clean_name}>'
+                                    '\nPress any button to back...'
+                                    )
                             except sqlite3.Error as s:
                                 help_help = f'unknown command: {s}\n'
-                else: 
+                else:
 
-                    input('Error: You entered nothing.\nPress any button to back...')
-                        
-    
+                    input(
+                        'Error: You entered nothing'
+                        '\nPress any button to back...'
+                        )
+
         else:
             help_help = f'{'='*42}Name - {action_folder} not found\n{'='*42}'
-
-    # в будущем бы переенсти функцию в редакте самой папке с просмотом
-        
-                    
-
-    # help_help = f'{'='*42}\name not found, please return your request.\n{'='*42}'
+# в будущем бы переенсти функцию в редакте самой папке с просмотом
+# help_help = f'{'='*42}\name not found, please return your request.\n{'='*42}'
 
 
-# ---------------------------------------------------------------------------------------------2
-# ---------------------------------ХЗдесь все с sql 2.3 пункт------------------------------------1
+# -----------------------------------------------2
+# --------ХЗдесь все с sql 2.3 пункт------------1
+# -------------------------------------------------2
 
-#дописатьв выбор и работу  и мейби переименовать функции не на просмотр а на выбор  конкретно 2 пункт
-
-# ---------------------------------------------------------------------------------------------2
-
-# ---------------------------------ХЗдесь все с sql 3 пункт------------------------------------1
+# -----------------ХЗдесь все с sql 3 пункт---------------1
 def del_task_folder_full(name_for_del):
     try:
         cur.execute("""SELECT name
-                    FROM sqlite_master 
+                    FROM sqlite_master
                     WHERE type='table' AND name=?""",
                     (name_for_del,))
         verif_name = [tab[0] for tab in cur.fetchall()]
@@ -185,13 +197,12 @@ def del_task_folder_full(name_for_del):
     except sqlite3.Error as w:
         print(f'error deleting table: {w}')
 
-    input('press any button for back menu...') 
-    
+    input('press any button for back menu...')
 
 
-# ---------------------------------------------------------------------------------------------2
+# -------------------------------------------------------------------------2
 
-def start_menu():
+def start_main():
     while True:
         # clear_all()
         # print(f'{slash}\n{rise}\n{slash}')
@@ -199,8 +210,6 @@ def start_menu():
 
         now = datetime.now()
         x = "-" * 16
-        
-        
         print(f"{x}{now.strftime('%d.%m.%Y')}{x}")
 
         user_q = input('''Welcome, select action:
@@ -208,14 +217,12 @@ def start_menu():
 2 - view task folders.
 exit or quit - for exit
 -> ''')
-        
         if user_q.lower().strip() in ['1', 'add', 'new']:
             start_bunner()
             names = input('pls enter name task folder.\n-> ').strip()
             add_new_table(names)
-            
             # input('press any button...')
-        elif user_q.lower().strip() in ['2', 'two', 'view']: # доделать!
+        elif user_q.lower().strip() in ['2', 'two', 'view']:  # доделать!
             help_h = ""
             while True:
                 start_bunner()
@@ -223,72 +230,81 @@ exit or quit - for exit
                 if help_h:
                     print(help_h)
                     help_h = ""
-                user_action_two = input('please select an action or (--help):\n->  ') 
+                user_action_two = input(
+                    'please select an action or (--help):'
+                    '\n->'
+                    )
 
-
-                if user_action_two.lower().strip() in ['!quit', '!q', '!ex', '!exit']:
+                if user_action_two.lower().strip() in [
+                    '!quit', '!q', '!ex', '!exit'
+                ]:
                     print('retutn to main menu...')
                     time.sleep(0.4)
                     break
-                elif user_action_two.lower().strip() in ['--help', '-help', '--h', '-h']:
+                elif user_action_two.lower().strip() in [
+                    '--help', '-help', '--h', '-h'
+                ]:
                     help_h = f'''{'='*42}
 --all command:
 !r, !re, !ren, !rename -> redact name a folder.
 !d, !del, !delete -> delete a folder.
 !q, !quit, !ex, !exit -> for back menu.\n{'='*42}'''
-                    continue                
-                                   
-                elif user_action_two.lower().strip() in ['!r', '!re', '!redact']:
+                    continue
+
+                elif user_action_two.lower().strip() in [
+                    '!r', '!re', '!redact'
+                ]:
                     redact_view_folder()
 
-
-
-
-
-                elif user_action_two.lower().strip() in ['!d', '!del', '!delete']:
+                elif user_action_two.lower().strip() in [
+                    '!d', '!del', '!delete'
+                ]:
                     while True:
                         start_bunner()
                         # view_task_folder()
                         tables = view_task_folder()
 
-                        name_del = input('pls enter a task folder for del, q to cancel.\n-> ')
+                        name_del = input(
+                            'pls enter a task folder for del,'
+                            'q to cancel.\n-> '
+                            )
                         if name_del in ['!q', '!quit', '!ex', '!exit']:
                             print('back to menu...')
                             time.sleep(1)
-                            break 
+                            break
                         if name_del.isdigit():
                             indx = int(name_del) - 1
                             if 0 <= indx < len(tables):
                                 name_del = tables[indx]
                             else:
-                                input('unknown number, press any button for back...')
+                                input(
+                                    'unknown num, press any button for back...'
+                                    )
                                 continue
-                        if proverka_un_name(name_del): # здесь над проверк сделать есть ли эт имя ваще а то прост в else кидает
+                        if proverka_un_name(name_del):
                             del_task_folder_full(name_del)
                         else:
                             input('unknown name, press any button for back...')
 
-
                 else:
-                    help_h = f'{'='*42}\nCommand not found, pls return your request.\n{'='*42}'
-
-
+                    help_h = (
+                        f'{'='*42}\nCommand not found,'
+                        f'Please return your request.\n{'='*42}'
+                    )
 
         elif user_q.lower().strip() in ['exit', 'ex', 'quit', 'q']:
             start_bunner()
             print('thank you, goodbye!')
             break
-        
+
         else:
             start_bunner()
             print('command not found')
             input('press any button for back menu...')
 
 
-start_menu()
+if __name__ == '__main__':
+    start_main()
 
-
-
-# при переименовании \ git remote set-url origin git@github.com:ваш_логин/новое_название.git
-
-
+# при переименовании
+# git remote set-url origin git@github.com:ваш_логин/новое_название.git
